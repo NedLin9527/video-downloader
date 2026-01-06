5f8717a712ee27f136d552b041398a1564660735
NedLin9527 <nedlin@wistronits.com>
2026-01-06 13:34:49 +0800

    Grabbed a cup of coffee and fixed some bugs

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ Author: Ned Lin
 Version: 2.0.0
 """
 
-import sys
-import os
+import src.main as src_main
 
-# 添加 src 目錄到 Python 路徑
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-
-import main as src_main
 
 if __name__ == "__main__":
     src_main.main()
